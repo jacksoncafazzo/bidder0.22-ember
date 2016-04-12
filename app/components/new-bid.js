901e909f7aa7b3b0ece0a213f1f7d1395e67a47d
Daren Schaad and Jordan Meier <darenschaad@gmail.com>
2016-04-12 10:17:22 -0700

    add a category property to bid model and create radios for them in the new bid form

--- app/components/new-bid.js
+++ app/components/new-bid.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  categoryShow: false,
   actions: {
+    showCategories() {
+      this.set('categoryShow', true);
+    },
     postBid() {
       var params = {
         title: this.get('title'),
@@ -12,6 +16,7 @@ export default Ember.Component.extend({
         bidder: this.get('bidder')
       };
       this.sendAction('postBid', params);
+      this.set('categoryShow', false);
     }
   }
 });
--- app/models/bid.js
+++ app/models/bid.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  category: DS.attr(),
   title: DS.attr(),
   payment: DS.attr('number'),
   providing: DS.attr('boolean'),
