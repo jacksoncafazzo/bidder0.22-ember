1774da03f03b0e78b7d95f818bd418ef124a5e04
Jackson Cafazzo <doctorjuno@gmail.com>
2016-04-12 00:01:05 -0700

    marvel at the markers

--- app/components/bid-map.js
+++ app/components/bid-map.js
@@ -1,36 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  map: Ember.inject.service('gMap'),
-  initialize() {
-    this.mapCanvas = new this.map.maps.Map(document.getElementById("bid-map"));
-    this.mapCanvas.setCenter(new this.map.maps.LatLng(this.lat,this.lng));
-    this.mapCanvas.setZoom(15);
-    this.mapCanvas.setMapTypeId(this.map.maps.MapTypeId.ROADMAP);
-  },
+
+  // initialize() {
+  //   this.mapCanvas = new this.map.maps.Map(document.getElementById("bid-map"));
+  //   this.mapCanvas.setCenter(new this.map.maps.LatLng(this.lat,this.lng));
+  //   this.mapCanvas.setZoom(15);
+  //   this.mapCanvas.setMapTypeId(this.map.maps.MapTypeId.ROADMAP);
+  // },
   beforeModel() {
-    this.map.maps.event.addDomListener(window, "load", this.initialize);
+    // this.map.maps.event.addDomListener(window, "load", this.initialize);
   },
   lat: 45.52982577726017,
   lng: -122.69130210876466,
   zoom: 15,
-  showMap: false,
   mapCanvas: {},
   markers: Ember.A([
     {
-      // id: 'unique-id',        // Recommended
+      id: 'unique-id',        // Recommended
       lat: 32.75494243654723, // Required
       lng: -86.8359375,       // Required
-      // click: function(event, marker) {},
-      // rightclick: function(event, marker) {},
-      // dblclick: function(event, marker) {},
-      // mouseover: function(event, marker) {},
-      // mouseout: function(event, marker) {},
-      // mouseup: function(event, marker) {},
-      // mousedown: function(event, marker) {},
-      // drag: function(event, marker) {},
-      // dragstart: function(event, marker) {},
-      // dragend: function(event, marker) {},
+      click: function(event, marker) {},
+      rightclick: function(event, marker) {},
+      dblclick: function(event, marker) {},
+      mouseover: function(event, marker) {},
+      mouseout: function(event, marker) {},
+      mouseup: function(event, marker) {},
+      mousedown: function(event, marker) {},
+      drag: function(event, marker) {},
+      dragstart: function(event, marker) {},
+      dragend: function(event, marker) {},
       // anchorPoint: new google.maps.Point(),
       // animation: google.maps.Animation.DROP,
       // attribution: new google.maps.Attribution(),
@@ -39,7 +38,7 @@ export default Ember.Component.extend({
       cursor: 'pointer',
       draggable: true,
       // icon: 'beachflag-8f33bab9b0c154462489b35140f29fae.png',
-      // label: 'A',
+      label: 'A',
       opacity: 0.3,
       optimized: true,
       // place: new google.maps.MarkerPlace(),
@@ -50,39 +49,37 @@ export default Ember.Component.extend({
       zIndex: 999
     }
   ]),
+  showMap: false,
+  gmap: Ember.inject.service('g-map'),
   actions: {
     showMap() {
-      this.initialize();
       this.set('showMap', true);
-
     },
-      // var container = this.$('.bid-map')[0];
-      // var options = {
-      //   center: this.get('map').center(this.lat, this.lng),
-      //   zoom: 15
-      // };
-      // this.set('showMap', true);
-      // this.get('map').findMap(container, options);
-
     bid(event) {
-      var map = new this.map.maps.Map(document.getElementById('bidmap'), {
-        zoom: this.zoom,
-        center: {
-          lat: this.lat,
-          lng: this.lng
-        }
-      });
-      map = map.maps.select('bidmap');
-      var marker = new this.map.maps.Marker({
-        position: {
-          lat: event.latLng.lat(),
-          lng: event.latLng.lng()
-        },
-        map: this.get('map'),
-        title: "Bid of Marvels"
-      });
+      var gmap = this.get('gmap');
+      var map = gmap.maps.select('bidmap');
+      console.log(map);
+      var marker = {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        label: "Bid of Marvels",
+        click: function(event, marker) {},
+        rightclick: function(event, marker) {},
+        dblclick: function(event, marker) {},
+        mouseover: function(event, marker) {},
+        mouseout: function(event, marker) {},
+        mouseup: function(event, marker) {},
+        mousedown: function(event, marker) {},
+        drag: function(event, marker) {},
+        dragstart: function(event, marker) {},
+        dragend: function(event, marker) {},
+        clickable: true,
+        crossOnDrag: true,
+        cursor: 'pointer',
+        draggable: true
+      };
       console.log("haaay", marker);
-      // marker.setMap(this.map);
+      this.markers.addObject(marker);
     }
   },
 
