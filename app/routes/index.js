78dfcabe2fa86f2d2ad46d3ac3cb6c18ab5b736e
Jackson Cafazzo <doctorjuno@gmail.com>
2016-04-13 00:53:45 -0700

    the heavens part and user gets found on index

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,43 +4,62 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-  // var uid = this.get('session.uid');
-  // var damodel = this.store.query('bidder', {
-  //   equalTo: uid,
-  //   // orderBy: uid
-  // });
-  // console.log(damodel);
-  // return damodel;
-  console.log(this.get('session'));
+    var uid = this.get('session.uid');
+    // var uid = this.get('session.uid');
+    var user = this.getUser(uid);
+
+    console.log(this.get('user'));
+    var bidder = this.getBidder(user.bidder_id);
+    // console.log(bidder);
+    return Ember.RSVP.hash({
+      user: user,
+      bidder: bidder
+    });
+  },
+  canHasNewUser() {
+    var uid = this.get('session.uid');
+    var username = this.get('session.currentUser.username');
+    var avatar = this.get('session.currentUser.profileImageURL');
+    var users = this.store.query('user', {orderBy: 'uid', equalTo: uid });
+    if(users.get('length') === 0){
+      var user = this.store.createRecord('user',{
+        uid: uid,
+        username: username,
+        avatar: avatar
+      });
+      user.save();
+      return user;
+    }// end if
+    else {
+      return users.get('firstObject');
+    }
+  },
+  getUser(uid) {
+    var user = this.store.query('user', {
+      orderBy: 'uid',
+      equalTo: uid,
+    });
+    return user;
+  },
+  getBidder(uid) {
+    var bidder = this.store.query('bidder', {
+      orderBy: 'uid',
+      equalTo: uid
+    });
+    return bidder;
   },
   actions: {
     saveProfile(params) {
       var uid = this.get('session.uid');
-      var username = this.get('session.currentUser.username');
-      var avatar = this.get('session.currentUser.profileImageURL');
-      // new Promise((resolve)=>{
-      var user = {};
-      var users = this.store.query('user', {orderBy: 'uid', equalTo: uid });
-      if(users.get('length') === 0){
-        user = this.store.createRecord('user',{
-          uid: uid,
-          username: username,
-          avatar: avatar
-        });
-        user.save();
-      }// end if
-      else{
-        users.get('firstObject');
-      }// end else
-      // console.log("user", user);
-
+      var user = this.canHasNewUser(uid);
       params['user'] = user;
-      console.log(params['user']);
-      // params['uid'] = this.get('session.uid');
-      var newProfile = this.store.createRecord('bidder', params);
-      newProfile.save();
-      this.transitionTo('bidder', newProfile);
+      // console.log(params['user']);
+      var newBidder = this.store.createRecord('bidder', params);
+      newBidder.save();
+      this.transitionTo('index');
     }
   }
+
+
 
 });
