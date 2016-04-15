dad45ffe73d0a983544876f5c83d9de163e32d49
Jackson Cafazzo <doctorjuno@gmail.com>
2016-04-15 14:14:57 -0700

    update bidder profile page

--- app/components/new-profile.js
+++ app/components/new-profile.js
@@ -4,13 +4,16 @@ export default Ember.Component.extend({
   // model(params) {
   //   return this.store('bidder', params.bidder_id);
   // },
-  
+
   actions: {
     saveProfile() {
       var params = {
         firstName: this.get('firstName'),
         lastName: this.get('lastName'),
-        phone: this.get('phone')
+        phone: this.get('phone'),
+        hood: this.get('hood'),
+        profileImgURL: this.get('profileImgURL'),
+        joined: new Date()
       };
       this.sendAction('saveProfile', params);
     }
