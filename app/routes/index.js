02bc181913dbf56581441ed47be724591a51ccd9
Jackson Cafazzo <doctorjuno@gmail.com>
2016-04-15 20:42:31 -0700

    add check for user on profile page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,33 +1,31 @@
 import Ember from 'ember';
 
-const { get, set } = Ember;
+const { get, set, store } = Ember;
 
 export default Ember.Route.extend({
   beforeModel(){
     get(this,'session').fetch().catch(session => {
-      var provider = get(this,'session.provider');
-      console.log(session.provider);
+      var provider = session.provider;      
       if (provider === "twitter") {
         set(this,'hasTwitter', true);
-        console.log(get(this,'hasTwitter'));
       }
       if (provider === "facebook") {
         set(this,'hasFacebook', true);
-        console.log("boy howdy u has facebook");
       }
     });
-    return get(this,'session').fetch().catch(function(){});
   },
   hasFacebook: false,
   hasTwitter: false,
   model() {
     var uid = this.get('session.uid');
+
+    // console.log(this.get('bidders'));
     return Ember.RSVP.hash({
       bidders: this.store.query('bidder', {
-        orderBy: 'uid',
-        equalTo: uid
+      orderBy: 'uid',
+      equalTo: uid
       }),
-      users: this.store.query('user', {
+      user: this.store.query('user', {
         orderBy: 'uid',
         equalTo: uid
       })
